019b3567b2c62aec9738b8fe321f197d28781e3f
devmilon923 <dev.milon923@gmail.com>
2026-06-18 13:37:05 +0600

    added basic socket middleware

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,18 +1,31 @@
 import { Server as HttpServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
 
+let io: SocketIOServer;
+let socketUsers = new Map<
+  string, // this string is db userId
+  { socketId: string; name: string; email: string }
+>();
 export async function socketServerInstance(httServer: HttpServer) {
   const { Server } = await import("socket.io");
 
-  const io = new Server(httServer, {
+  // socket instance create
+  io = new Server(httServer, {
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
       credentials: true,
     },
   });
-  console.log("Socket server is running");
+  // middleware
+  io.use((socket, next) => {
+    console.log(socket);
+    next();
+  });
+  // connection
   io.on("connection", (socket) => {
     console.log(socket.id, "connected");
     socket.on("disconnect", () => console.log(socket.id, "disconnected"));
   });
 }
+export { io };
